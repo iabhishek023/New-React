0d9de5658a873653fa1debfce29fb438dde9e4b1
iabhishek023 <iabhishek023@gmail.com>
2024-10-10 18:30:21 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,22 @@
+const heading=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h2",{},"I am h2 tag"),
+      ]
+    ), 
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h2",{},"I am h2 tag"),
+      ]
+    ), 
+    React.createElement("div",{class:"children"},[
+        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("p",{},"I am h2 tag"),
+    ]), 
+);
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
